4443d6c99e072aae83143de4ae8253099505a5f3
Pranavi Reddy <46841618+Pranavireddy30@users.noreply.github.com>
2020-01-10 22:16:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var nodemailer = require('nodemailer');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var session = require('express-session');
 
 var app = express();
 
@@ -21,6 +22,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/accountant',accountantRouter);
+app.use('/admin',adminRouter);
+app.use(session({
+  secret: 'food chef',
+   resave: false,
+   store: sessionStore,
+   saveUninitialized: false,
+  // cookie: { secure: true}
+}));
+// passport code
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use('accountant',new LocalStrategy(accountant.authenticate()));
+passport.use('admin',new LocalStrategy(admin.authenticate()));
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  if(user!=null)
+   done(null, user);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
