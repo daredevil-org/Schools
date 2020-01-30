f854356e4ce43f93317650fdc014ddec7d2c71a6
amruthakanumuri <59133182+amruthakanumuri@users.noreply.github.com>
2020-01-30 19:16:44 +0530

    Update student.js

--- models/student.js
+++ models/student.js
@@ -1,1 +1,32 @@
 var mongoose = require('mongoose');
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var studentSchema = new schema({
+    firstname:{
+        type:String,
+        required:true,
+    },
+    lastname:{
+        type:String,
+        required:true,
+    },
+    fathername:{
+        type:String,
+        required:true,
+    },
+    phone_no:{
+        type:Number,
+        required:true,
+        },   
+    email:{
+        type:String,
+        required:true,
+        },
+    rollno:{
+        type:Number,
+        required:true,
+    },         
+})
+
+module.exports = mongoose.model('student',studentSchema);
