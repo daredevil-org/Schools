d1090713c0720e34dd0f94ad66dd01f596be073a
Eshwar Grandhe <44287126+Eshwar-Grandhe@users.noreply.github.com>
2020-02-29 19:07:19 +0530

    Update student.js

--- models/student.js
+++ models/student.js
@@ -4,28 +4,22 @@ var schema = mongoose.Schema;
 var studentSchema = new schema({
     firstname:{
         type:String,
-        required:true,
     },
     lastname:{
         type:String,
-        required:true,
     },
     fathername:{
         type:String,
-        required:true,
     },
     phone_no:{
         type:Number,
-        required:true,
         },   
     email:{
         type:String,
-        required:true,
         },
     rollno:{
         type:Number,
-        required:true,
     },         
 })
 
-module.exports = mongoose.model('student',studentSchema);
+module.exports = studentSchema;
