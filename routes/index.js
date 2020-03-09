e26a75a2500d369c03c6f61fb278e8323400a692
daredevil555 <46808885+daredevil555@users.noreply.github.com>
2020-03-09 19:50:27 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -73,7 +73,7 @@ router.post('/forgot_admin', function(req, res, next) {
         subject: 'Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset_admin/' + token + '\n\n' +
+          'https://sklsystem.herokuapp.com'  + '/reset_admin/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
       smtpTransport.sendMail(mailOptions, function(err) {
